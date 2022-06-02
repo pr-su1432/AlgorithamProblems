b31f7fedbb70dc3368d723735518dfea06f44601
[prasanna] <prasanna>
2022-06-02 16:51:49 +0530

    [prasanna]; booble sorting of a integers

--- AlgorithamProblems/Program.cs
+++ AlgorithamProblems/Program.cs
@@ -4,23 +4,17 @@ namespace AlgorithamProblems
 {
     class program
     {
-      
-        public static void Main(String[] args)
+   
+
+        static void Main(String[] args)
         {
-
-            string[] names = { "prasanna lakshmi", "lakshmi prasanna", "vara lakshmi", "lakshmi vara" };
-            AlgorithamProblems.InsertSort value = new AlgorithamProblems.InsertSort();
-            value.InsertionSort(names);
-            foreach (var item in names)
-            {
-                Console.WriteLine(item);
-            }
-
-
-
+            
+            
+            AlgorithamProblems.BoobleSort value = new AlgorithamProblems.BoobleSort();
+            value.BoobleSorting();
+            Console.Read();
         }
-
-        
     }
+    
 }
 
